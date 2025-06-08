343b505def7a0866489efa31e6f68abee4235bd6
Ak-soni2 <akshaypratap270@gmail.com>
2025-06-08 18:53:33 +0530

    resolve conflicts

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -49,7 +49,7 @@ const Sidebar = () => {
           <Link to="/" className="flex items-center gap-2.5" onClick={() => setIsOpen(false)}>
             <ShipWheelIcon className="size-9 text-primary" />
             <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
-              Streamify
+              Stone Chat
             </span>
           </Link>
         </div>
